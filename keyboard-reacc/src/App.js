9659b44194a19c98101d20db895353cfda8c4e20
Hrithvik  Alex <halex623@v1022-rn-238-248.campus-dynamic.uwaterloo.ca>
2018-11-21 23:56:44 -0500

    finished UI component

--- keyboard-reacc/src/App.js
+++ keyboard-reacc/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './Button'; 
 
@@ -8,9 +7,14 @@ class KeyBoard extends Component{
     super(props);
     this.state = {
         buttons: Array(63).fill(null),
+        grid: Array(63).fill(0).map(() => Array(8).fill(0))
     }
   }
   
+
+  /* This portion of the code will be used to initialized the keyboard UI, set button values, and initialize
+  the coordinate grid.*/
+
   keyInit() {
     //INITIALIZING EACH KEY
     const everyKey = "`1234567890-=__qwertyuiop[]\\_asdfghjkl;'__zxcvbnm,./__"
@@ -34,61 +38,167 @@ class KeyBoard extends Component{
     this.state.buttons[62] = "right";
   }
 
-  handleClick(i) {
-    console.log("YEET U CLICKED " + this.state.buttons[i]);
-  }
-
-  renderStuff() {
-    return array.map((item, i) => {
-      let flag = false;
-      if (i == 14 || i == 28 || i == 41 || i == 53) {
-        flag = true;
-      }
-      return (
-        flag ? </div><div className="board-row"> {this.renderButton(i)}
-        :
-        {this.renderButton(i)};
-      )
-    })
-  }
-
-_onMouseMove(e) {
-  const { screenX, screenY } = e;
-  console.log(`x: ${screenX} y: ${screenY}`);
-  }
-  
-
-  renderButton(i) {
-    let width = null;
-    let height = null;
+  buttonWidth(i) {
+    //SETTING WIDTHS OF BUTTON
+    let width = 87;
     switch(i) {
-      case 13: case 14: case 28: case 41: case 52:
-        width = 160;
+      case 13: case 14:  
+        width = 149;
+        break;
+      case 28:
+        width = 175;
+        break;
+      case 40:
+        width = 148;
+        break;
+      case 41: case 52:
+        width = 205;
+        break;
+      case 56:
+        width = 365;
+        break;
+      case 53: case 54: case 55: case 57: case 58: case 60: case 61:case 59: case 62:
+        width = 114;
         break;
     }
+    return width;
+  }
+
+  buttonX(i) {
+    let x = 0;
+    let widthcounter = 0;
+    for(let j = 0; j < 63; j++){
+      if(j == 14 || j == 28 || j== 41 || j==52){
+        widthcounter = 0;
+      }
+      widthcounter += this.buttonWidth(j);
+      if(j==i){
+        x = widthcounter;
+        break;
+      }
+    }
+    return x;
+  }
+
+  buttonY(i) {
+    let y = 0;
+    if(i>13){
+      y = 160;
+    } else if(i>27){
+      y = 320;
+    } else if(i>40){
+      y = 480;
+    } else if(i>52){
+      y = 640;
+    }
+    return y;
+  }
+
+
+  renderButton(i) {
+    let x = this.buttonX(i);
+    let y = this.buttonY(i);
+    let width = this.buttonWidth(i);
+    let height = 160;
+    if(i == 60 || i == 61) {
+      height = 80;
+    }
+    
+    //GRID INIT
+    let fullCoords = Array(8).fill(0);
+		fullCoords[0] = x;
+		fullCoords[1] = x + width;
+		fullCoords[2] = x;
+		fullCoords[3] = x + width;
+		fullCoords[4] = y;
+		fullCoords[5] = y;
+		fullCoords[6] = y - height;
+		fullCoords[7] = y - height;
+    this.state.grid[i] = fullCoords;
+
       return (
         <Button 
         value={this.state.buttons[i]}
         onClick={() => this.handleClick(i)}
         width={width}
         height={height}
+        x={x}
+        y={y}
         />
-      
       )
     }
+
+  renderStuff() {
+    return this.state.buttons.map((item, i) => {
+      let flag = false;
+      if (i == 13 || i == 27 || i == 40 || i == 52) {
+        flag = true;
+      }
+      
+      if(flag) {
+        return (
+          <div className="board-row"> {this.renderButton(i)} </div>
+        )
+      } else if(i == 60) {
+        return (
+          <div className="grid">
+            <div className="board-column"> 
+              {this.renderButton(i)}
+              {this.renderButton(i+2)}
+            </div>
+            <div id="downButton"className="board-column">
+              {this.renderButton(i+1)}
+            </div>
+          </div>
+          
+        )
+      }
+      else if(i == 61 || i==62){
+        return(
+          <div></div>
+        )
+      }
+        else {
+          return (
+            <div>{this.renderButton(i)}</div> 
+          )
+        }
+      
+    })
+  }
+
+
+/* This portion of the code will handle the logic behind handling normal keyboard and swipe functionality. */
+
+
+/* _onMouseMove(e) {
+  const { screenX, screenY } = e;
+  console.log(`x: ${screenX} y: ${screenY}`);
+   } */
   
 
+
+   handleClick(i) {
+    console.log("YEET U CLICKED " + this.state.buttons[i]);
+  }
+
+
+
+
+
+  
   render() {
     this.keyInit();
 
     return (
-      <div className="container" onMouseMove={this._onMouseMove.bind(this)}>
+      <div className="container" /*onMouseMove={this._onMouseMove.bind(this)}*/>
         <div className="board-row">
-          {renderStuff()};
+          {this.renderStuff()}
       </div>
-    )}
+      </div>
+    )
     
-
+}
 }
 
 class Project extends Component{
--- keyboard-reacc/src/Button.js
+++ keyboard-reacc/src/Button.js
@@ -4,22 +4,48 @@ export default class Button extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			width: 80,
+			width: 87,
 			height: 160,
+			x: 0,
+			y: 0,
 		}
-		const { width, height } = this.props;
+		const { width, height, x, y } = this.props;
 
 		if (width) {
 			this.state.width = width;
 		}
 		if (height) {
 			this.state.height = height;
+		} 
+		if (x) {
+			this.state.x = x;
+		}
+		if (y) {
+			this.state.y = y;
 		}
 	}
+
+	getCoords() {
+		let fullCoords = Array(8).fill(0);
+		fullCoords[0] = this.state.x;
+		fullCoords[1] = this.state.x + this.state.width;
+		fullCoords[2] = this.state.x;
+		fullCoords[3] = this.state.x + this.state.width;
+		fullCoords[4] = this.state.y;
+		fullCoords[5] = this.state.y;
+		fullCoords[6] = this.state.y - this.state.height;
+		fullCoords[7] = this.state.y - this.state.height;
+		return fullCoords;
+	}
+
 	render() {
 		const { value, onClick } = this.props;
+		let buttonStyle = {
+			width: this.state.width,
+			height: this.state.height
+		}
 	   return (
-	    <button className="button" onClick={onClick}>
+	    <button className="button" onClick={onClick} style={buttonStyle}>
 	      {value}
 	    </button>
   		);
